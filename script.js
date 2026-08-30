31be163047459cc2aa0e3ed0290a7fb13227f8fb
k2rEiGhT <126437381+k2rEiGhT@users.noreply.github.com>
2026-08-30 09:54:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -848,8 +848,12 @@ const opponentTeams = [
   {
     name: "未登録",
     players: [
-      { num: "1", name: "A", isStarter: true },
-      { num: "2", name: "B", isStarter: true }
+      { num: "1", name: "", isStarter: true },
+      { num: "2", name: "", isStarter: true },
+      { num: "3", name: "", isStarter: true },
+      { num: "4", name: "", isStarter: true },
+      { num: "5", name: "", isStarter: true },
+      { num: "6", name: "", isStarter: true },
     ]
   }
 ];
